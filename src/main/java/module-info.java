e8c6f1ce2bb499e8dd01cedda2b51f8a8ea7bcb1
tomasz-nyckowiak <tomasz.nyckowiak.programista@gmail.com>
2022-08-07 12:58:26 +0200

    "Start"

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.weather.pogodynka {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.weather.pogodynka to javafx.fxml;
+    exports com.weather.pogodynka;
+}
